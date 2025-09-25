723dddaa1a37d4c6d217b16e37759f290000650e
SirZefar <85900402+SirZefar@users.noreply.github.com>
2025-09-24 22:54:19 -0700

    [UPDATE]--> FavList localStorage

--- resources/views/livewire/product-card.blade.php
+++ resources/views/livewire/product-card.blade.php
@@ -1,21 +1,30 @@
 <div class="animate-fade-in">
     <a class="group bloc">
-        <div class="relative overflow-hidden rounded-lg bg-white shadow-md transition-all duration-300 ease-out hover:translate-y-[-5px] hover:shadow-xl">
+        <div
+            class="relative overflow-hidden rounded-lg bg-white shadow-md transition-all duration-300 ease-out hover:translate-y-[-5px] hover:shadow-xl">
             <div class="relative aspect-[3/4] overflow-hidden bg-gray-100">
-                <img class="absolute inset-0 h-full w-full object-contain transition-transform duration-500 scale-100" src="{{asset($productImg)}}" alt="">
-                <span class="absolute top-2 left-2 bg-black/80 text-white text-xs px-2 py-1 rounded">{{ $rareza }}</span>
-                <div class="absolute inset-0 bg-black/40 flex items-center justify-center hover:opacity-100 opacity-0 transition-opacity duration-300">
+                <img class="absolute inset-0 h-full w-full object-contain transition-transform duration-500 scale-100"
+                    src="{{ asset($productImg) }}" alt="">
+                <span
+                    class="absolute top-2 left-2 bg-black/80 text-white text-xs px-2 py-1 rounded">{{ $rareza }}</span>
+                <div
+                    class="absolute inset-0 bg-black/40 flex items-center justify-center hover:opacity-100 opacity-0 transition-opacity duration-300">
                     <div class="flex gap-2">
-                        <button class="h-10 w-10 rounded-full bg-white text-black hover:bg-white/90 hover:cursor-pointer" wire:click="$js.addToFav">
+                        <button
+                            class="h-10 w-10 rounded-full bg-white text-black hover:bg-white/90 hover:cursor-pointer"
+                            wire:click="$js.addToFav">
                             <span class="h-4 w-4">♥️</span>
                             <span class="sr-only">Añadir a favoritos</span>
                         </button>
-                        <button class="h-10 w-10 rounded-full bg-white text-black hover:bg-white/90 hover:cursor-pointer" wire:click="addToCart">
+                        <button
+                            class="h-10 w-10 rounded-full bg-white text-black hover:bg-white/90 hover:cursor-pointer"
+                            wire:click="addToCart">
                             <span class="h-4 w-4">🛒</span>
                             <span class="sr-only">Añadir al carrito</span>
                         </button>
-                        <a href={{ Route('producto' , ['id' => $productId]) }}>
-                            <button class="h-10 w-10 rounded-full bg-white text-black hover:bg-white/90 hover:cursor-pointer">
+                        <a href={{ Route('producto', ['id' => $productId]) }}>
+                            <button
+                                class="h-10 w-10 rounded-full bg-white text-black hover:bg-white/90 hover:cursor-pointer">
                                 <span class="h-4 w-4">👁️‍🗨️</span>
                                 <span class="sr-only">Ver detalles</span>
                             </button>
@@ -31,7 +40,7 @@
                     {{ $nombre }}
                 </h3>
                 <div class="flex items-center justify-between">
-                    <span class="font-bold">${{$price}}</span>
+                    <span class="font-bold">${{ $price }}</span>
                 </div>
             </div>
         </div>
@@ -39,19 +48,30 @@
 </div>
 
 @script
-<script>
-    $js('addToFav' , ()=> {
-        let favList = localStorage.getItem('favList');
-        
-        if (!favList) {
-            localStorage.setItem('favList' , JSON.stringify([$wire.productId,]));
-        }
+    <script>
+        $js('addToFav', () => {
+            let favList = localStorage.getItem('favList');
 
-        if (favList) {
-            const list = JSON.parse(favList);
-            let newList = list.filter(idProd => idProd !== $wire.productId);
-            
-        }
-    })
-</script>
+            if (!favList) {
+                localStorage.setItem('favList', JSON.stringify([$wire.productId, ]));
+            }
+
+            if (favList) {
+                const currentFavList = JSON.parse(favList);
+                const currentProduct = $wire.productId;
+
+                const found = currentFavList.find((idProd) => currentProduct === idProd);
+
+                if (!found) {
+                    currentFavList.push(currentProduct);
+                    localStorage.setItem('favList', JSON.stringify(currentFavList));
+                }
+
+                if (found) {
+                    let newList = currentFavList.filter(idProd => idProd !== currentProduct);
+                     localStorage.setItem('favList', JSON.stringify(newList));
+                }
+            }
+        })
+    </script>
 @endscript
